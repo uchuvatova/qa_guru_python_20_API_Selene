547d8886aae27670086c06a7a851ed12ea4bc5e2
uchuvatova <109352814+uchuvatova@users.noreply.github.com>
2023-12-18 17:18:22 +0300

    добавлены тесты на добавление товаров в корзину

--- conftest.py
+++ conftest.py
@@ -1,46 +1,3 @@
-import os
-
-import pytest
-from dotenv import load_dotenv
-from selene import browser
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-
-from utils import attach
-'''
-@pytest.fixture(scope='session', autouse=True)
-def load_env():
-    load_dotenv()
-@pytest.fixture(scope='function')
-def setup_browser(request):
-    options = Options()
-    selenoid_capabilities = {
-        "browserName": "chrome",
-        "browserVersion": "100.0",
-        "selenoid:options": {
-            "enableVNC": True,
-            "enableVideo": True
-        }
-    }
-    options.capabilities.update(selenoid_capabilities)
-    login = os.getenv('LOGIN')
-    password = os.getenv('PASSWORD')
-    driver = webdriver.Remote(command_executor=f"https://{login}:{password}@selenoid.autotests.cloud/wd/hub",
-                              options=options)
-    browser.config.driver = driver
-    browser.config.base_url = 'https://demoqa.com'
-
-    yield browser
-
-    attach.add_screenshot(browser)
-    attach.add_logs(browser)
-    attach.add_html(browser)
-    attach.add_video(browser)
-
-    browser.quit()
-'''
-
-
 import pytest
 from selene.support.shared import browser
 from selenium.webdriver.chrome.options import Options
@@ -56,7 +13,7 @@ browser.config.driver = driver
 def browser_management():
     browser.config.set_value_by_js = True
     browser.config.timeout = 2.0
-    browser.config.window_width = 1400
-    browser.config.window_height = 1200
+    browser.config.window_width = 1280
+    browser.config.window_height = 1024
     yield
     browser.quit()
---
+++ tests_demoshop/test_cart.py
@@ -0,0 +1,72 @@
+import json
+import logging
+import random
+
+import requests
+from allure_commons._allure import step
+from allure_commons.types import AttachmentType
+from requests import Response
+from selene import browser
+from selene.support.conditions import have
+import allure
+from conftest import browser_management
+
+WEB_URL = "https://demowebshop.tricentis.com"
+API_URL = "https://demowebshop.tricentis.com"
+
+
+def demoshop_api_post(url, **kwargs):
+    with step("API Request"):
+        result = requests.post(url=API_URL + url, **kwargs)
+        allure.attach(body=result.request.url, name="Request url",
+                      attachment_type=AttachmentType.TEXT)
+        allure.attach(body=result.request.method, name="Request method",
+                      attachment_type=AttachmentType.TEXT)
+        allure.attach(body=json.dumps(result.request.body, indent=4, ensure_ascii=True), name="Request body",
+                      attachment_type=AttachmentType.JSON, extension="json")
+        allure.attach(body=json.dumps(result.json(), indent=4, ensure_ascii=True), name="Response body",
+                      attachment_type=AttachmentType.JSON, extension="json")
+        allure.attach(body=str(result.cookies), name="Response cookies", attachment_type=AttachmentType.TEXT,
+                      extension="txt")
+
+        logging.info(result.request.url)
+        logging.info(result.request.method)
+        logging.info(result.request.body)
+        logging.info(result.status_code)
+        logging.info(result.text)
+        logging.info(result.cookies)
+    return result
+
+
+def test_add_one_item_though_api(browser_management):
+    url = "/addproducttocart/catalog/45/1/1"
+
+    with step("Добавить товар из каталога через API"):
+        result: Response = demoshop_api_post(url)
+    with step("Получить cookie из API"):
+        cookie = result.cookies.get("Nop.customer")
+    with step("Передать cookie в браузер"):
+        browser.open(WEB_URL)
+        browser.driver.add_cookie({"name": "Nop.customer", "value": cookie})
+    with step("Открыть страницу Корзины покупок"):
+        browser.open(WEB_URL + "/cart")
+
+    browser.element(".qty-input").should(have.value("1"))
+
+
+def test_add_some_items_though_api(browser_management):
+    count_items = random.randint(1, 10)
+    url = f"/addproducttocart/catalog/45/1/{count_items}"
+
+    with step("Добавить несколько единиц товара из каталога через API"):
+        result: Response = demoshop_api_post(url)
+
+    with step("Получить cookie из API"):
+        cookie = result.cookies.get("Nop.customer")
+    with step("Передать cookie в браузер"):
+        browser.open(WEB_URL)
+        browser.driver.add_cookie({"name": "Nop.customer", "value": cookie})
+    with step("Открыть страницу Корзины покупок"):
+        browser.open(WEB_URL + "/cart")
+
+    browser.element(".qty-input").should(have.value(f"{count_items}"))
